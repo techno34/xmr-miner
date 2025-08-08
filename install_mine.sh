f1d92a93d7e9a25e48b9f45c1534e317fff58374
techno34 <65957988+techno34@users.noreply.github.com>
2025-08-08 11:45:21 +0530

    Update install_mine.sh

--- install_mine.sh
+++ install_mine.sh
@@ -8,8 +8,9 @@ echo "1. Installing dependencies..."
 pkg update -y || true
 pkg install -y wget tar proot
 
-echo "2. Downloading XMRig (linux-static-x64)..."
-wget -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.23.0/linux-static-x64.tar.gz
+echo "2. Downloading XMRig (linux-arm64)..."
+wget -O xmrig.tar.gz \
+https://github.com/xmrig/xmrig/releases/download/v6.23.0/xmrig-6.23.0-linux-arm64.tar.gz
 
 echo "3. Extracting..."
 tar -xzf xmrig.tar.gz
